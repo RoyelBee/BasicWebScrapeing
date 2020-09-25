820e0be88ae3cdb76ec3596ab0976ab673fd235e
Royel <royelsoft@gmail.com>
2020-09-25 18:14:00 +0600

    save fetched data as a csv file

--- initial_request.py
+++ initial_request.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests as re
 from bs4 import BeautifulSoup as bs
 url = "https://www.monster.com/jobs/search/?q=Developer&where=London"
@@ -10,6 +11,9 @@ results = soup.find(id="ResultsContainer")
 # Find element by class
 job_elements = results.find_all('section', class_='card-content')
 
+t = []
+c = []
+l = []
 # Sepaerate every job sections with  new lines and fetched actual data
 for job in job_elements:
     title = job.find('h2', class_='title')
@@ -17,8 +21,23 @@ for job in job_elements:
     location = job.find('div', class_='location')
     if None in (title, company, location):
         continue
-    print(title.text.strip())
-    print(company.text.strip())
-    print(location.text.strip())
-    print()
 
+    # print(title.text.strip())
+    # print(company.text.strip())
+    # print(location.text.strip())
+    # print()
+
+    # # ------ Insert all related data in a list
+    t.append(title.text.strip())
+    c.append(company.text.strip())
+    l.append(location.text.strip())
+
+
+df = pd.DataFrame()
+df['Job Title'] = t
+df['Company'] = c
+df['Location'] = l
+
+# print(df.head())
+df.to_csv('job_data.csv')
+print('Data Saved')
