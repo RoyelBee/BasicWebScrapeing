629017162c690cac8601650454ff62c206709e88
Royel <royelsoft@gmail.com>
2020-11-07 23:39:25 +0600

    Tag selection

--- workwith_html_file.py
+++ workwith_html_file.py
@@ -1,10 +1,22 @@
 from bs4 import BeautifulSoup
 
-file = open('./Data/test_html_file.html').read()
+file = open('./Data/three_sisters.html.html').read()
 # print(file)
 
 soup = BeautifulSoup(file, 'lxml')
 # print(soup)
 
 # print(soup.find('table'))
-print(soup.prettify())
+# print(soup.prettify())
+# print(soup)
+
+# Title
+# print(soup.title.prettify())
+
+# Paragraph (Default First p)
+# print(soup.p.prettify())
+
+# All paragraph
+body = soup.body
+for i in body.contents:
+    print(i if i is not None else '')
